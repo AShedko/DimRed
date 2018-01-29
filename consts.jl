90352178fb7fe5ed92cbb3265da64f7cfcf13fe9
AShedko <ayshedko@yandex.ru>
2018-01-29 20:20:33 +0300

    Added consts, Renamed clust to dist

---
+++ consts.jl
@@ -0,0 +1,10 @@
+const PATH = "/media/ashedko/_data_/datasets/UNC/"
+const PATH_Fmt = x -> PATH * string(x) * "/"
+const NSUBJ = 29 # 7 in test runs
+const SIZEX = 91
+const SIZEY = 109
+const SIZEZ = 91
+const SHAPE = (SIZEX,SIZEY,SIZEZ)
+const SIZE = SIZEX * SIZEY * SIZEZ
+const LEN = 3620
+const NEXP = 91
